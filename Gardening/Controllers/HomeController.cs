2b7d3ee0c0cd74b8976013c0a1bbda75b5ff7565
Laura <Lhuiefiction@gmail.com>
2019-03-27 09:27:33 -0400

    first commit

---
+++ Gardening/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gardening.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RecentObservations()
+        {
+            string API = "https://api.weather.gov/stations/KONZ/observations/latest";
+            HttpWebRequest request = WebRequest.CreateHttp(API);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            rd.Close();
+            string test;
+
+            return View();
+        }
+    }
+}
