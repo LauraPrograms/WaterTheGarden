e14e2e5b15c345b61e6aba57eb623a9cb4b5c653
Laura <Lhuiefiction@gmail.com>
2019-03-27 11:36:33 -0400

    checking past weather

--- Gardening/Controllers/HomeController.cs
+++ Gardening/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Gardening.APIKeys;
+using Gardening.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,7 +13,8 @@ namespace Gardening.Controllers
 {
     public class HomeController : Controller
     {
-       
+        //reference for later in getting soil temperature predictions https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?offset=500&limit=500
+        //reference to explain local codes https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?locationid=ZIP:48242
         public ActionResult Index()
         {
             return View();
@@ -42,5 +46,66 @@ namespace Gardening.Controllers
 
             return View();
         }
+
+        //will become it's own controller later once the database is made
+        //database will need to gather 5 years of data for March-November.
+        //build a predicting model of the 
+        public ActionResult PastObservations()
+        {
+            List<NOAAWeather> ChosenOnes = new List<NOAAWeather>();
+
+            APIKeyClass keys = new APIKeyClass();
+            string NOAAkey = keys.NKey;
+            string locationid = keys.locationA; //can change to other locations
+
+            for (int year = 2018; year <= DateTime.Today.Year; year++)
+            {
+                //this is a gardening app for watering, I don't need to water past November or before March
+                for (int month = 3; month <= 11; month++)
+                {
+                    if(month==4 && year==2019)
+                    {
+                        break;
+                    }
+                    string monthstring = "0" + month;
+                    monthstring = monthstring.Substring(monthstring.Length - 2, 2);
+
+                    string startDate = year + "-" + monthstring + "-01";
+                    //string endofMonth = GetDate(month);
+                    string endDate = year + "-" + monthstring + "-" + GetDate(month);
+
+                    //GHCND is the daily summaries
+                    string NOAAAPI = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&" + "locationid=" + locationid + "&startdate=" + startDate + "&enddate=" + endDate;
+                    HttpWebRequest WR = WebRequest.CreateHttp(NOAAAPI);
+                    WR.Headers.Add("token", NOAAkey);
+                    HttpWebResponse Response = (HttpWebResponse)WR.GetResponse();
+                    StreamReader rd = new StreamReader(Response.GetResponseStream());
+                    string data = rd.ReadToEnd();
+                    rd.Close();
+
+                    MetaWrapper AllEvents = JsonConvert.DeserializeObject<MetaWrapper>(data);
+                    ChosenOnes.AddRange(AllEvents.results.ToList());
+                }
+
+            }
+            ViewBag.Results = ChosenOnes;
+            return View(ChosenOnes);
+        }
+
+
+    public string GetDate(int month)
+    {
+        string endOfMonth;
+        if (month == 3 || month == 5 || month == 7 || month == 8 || month == 10)
+        {
+            return endOfMonth = "31";
+        }
+        else
+        {
+            return endOfMonth = "30";
+        }
     }
+
+
+}
 }
---
+++ Gardening/Models/NOAAWeather.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gardening.Models
+{
+    public class NOAAWeather
+    {
+        public string date { get; set; }
+        public string datatype { get; set; }
+        public string station { get; set; }
+        public string attributes { get; set; }
+        public string value { get; set; }
+    }
+
+    public class MetaWrapper
+    {
+        public List<NOAAWeather> results { get; set; }
+    }
+}
