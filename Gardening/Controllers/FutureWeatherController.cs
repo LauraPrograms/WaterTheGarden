6cb4ad063687d570cbbae079d1e6868f8bd0a252
Laura <Lhuiefiction@gmail.com>
2019-03-28 14:39:28 -0400

    Adding in forecasting data - needs a header

---
+++ Gardening/Controllers/FutureWeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Gardening.APIKeys;
+using Gardening.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gardening.Controllers
+{
+    public class FutureWeatherController : Controller
+    {
+        // GET: FutureWeather
+        public ActionResult Rain()
+        {
+            APIKeyClass api = new APIKeyClass();
+            string ForcastAPI = api.APIForecast;
+            HttpWebRequest request = WebRequest.CreateHttp(ForcastAPI);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            rd.Close();
+
+            MetaPeriods WeeklyForecast = JsonConvert.DeserializeObject<MetaPeriods>(data);
+            ViewBag.Results = WeeklyForecast;
+            return View();
+        }
+    }
+}
---
+++ Gardening/Models/Forecast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gardening.Models
+{
+    public class Forecast
+    {
+        public string number { get; set; }
+        public int EventNumber { get { return int.Parse(number); } }
+        public string name { get; set; }
+        public string detailedForecast { get; set; }
+    }
+
+    public class MetaPeriods
+    {
+        public List<Forecast> periods { get; set; }
+    }
+}
